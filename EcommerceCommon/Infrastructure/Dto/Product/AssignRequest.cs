ce558cbe7b54cf0d4c415568ef65517fff4c8d64
Tình Nguyễn Bá <batinhypbn@gmail.com>
2020-12-23 09:31:58 +0700

    commit

---
+++ EcommerceCommon/Infrastructure/Dto/Product/AssignRequest.cs
@@ -0,0 +1,13 @@
+﻿using EcommerceCommon.Infrastructure.Dto.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EcommerceCommon.Infrastructure.Dto.Product
+{
+    public class AssignRequest
+    {
+        public Guid Id { get; set; }
+        public List<SelectedItem> Assigns { get; set; } = new List<SelectedItem>();
+    }
+}
