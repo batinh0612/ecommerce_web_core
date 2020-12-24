85c236c55f86f28e123fd9af7e6e596a8ada191b
batinhypbn <batinhypbn@gmail.com>
2020-12-24 08:22:41 +0700

    commit

---
+++ eTakaful.Core/Services/ProductColorService.cs
@@ -0,0 +1,16 @@
+﻿using Ecommerce.Domain.Models;
+using Ecommerce.Repository.Interfaces;
+using Ecommerce.Service.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ecommerce.Service.Services
+{
+    public class ProductColorService : EcommerceServices<ProductColor>, IProductColorService
+    {
+        public ProductColorService(IRepository<ProductColor> baseReponsitory) : base(baseReponsitory)
+        {
+        }
+    }
+}
---
+++ eTakaful.Core/Services/ProductSizeService.cs
@@ -0,0 +1,16 @@
+﻿using Ecommerce.Domain.Models;
+using Ecommerce.Repository.Interfaces;
+using Ecommerce.Service.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ecommerce.Service.Services
+{
+    public class ProductSizeService : EcommerceServices<ProductSize>, IProductSizeService
+    {
+        public ProductSizeService(IRepository<ProductSize> baseReponsitory) : base(baseReponsitory)
+        {
+        }
+    }
+}
